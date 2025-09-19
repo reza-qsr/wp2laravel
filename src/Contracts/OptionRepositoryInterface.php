cf14e77616e4234fecc8ba8f350c29d4eb280335
reza_qsr <reza_qsr@yahoo.com>
2025-09-19 20:05:33 +0330

    composer added

---
+++ src/Contracts/OptionRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RezaGhasempour\Wp2Laravel\Contracts;
+
+interface OptionRepositoryInterface
+{
+    public function get(string $key, $default = null);
+    public function set(string $key, $value): bool;
+    public function delete(string $key): bool;
+    public function add(string $key, $value, string $autoload = 'yes'): bool;
+}

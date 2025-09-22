2a4854d84323dbfaff6f54980ff460c8a8483525
reza_qsr <reza_qsr@yahoo.com>
2025-09-22 17:49:32 +0330

    Facade Config Added

--- src/Facades/Wp2Laravel.php
+++ src/Facades/Wp2Laravel.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace RezaQsr\Wp2Laravel\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Wp2Laravel extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return \RezaQsr\Wp2Laravel\Wp2LaravelManager::class;
+    }
+}

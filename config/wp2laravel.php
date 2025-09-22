2902fa1d6b4f38a32ebc970ab11a8aeeb37e1be1
reza_qsr <reza_qsr@yahoo.com>
2025-09-22 17:54:09 +0330

    Config Modified

--- config/wp2laravel.php
+++ config/wp2laravel.php
@@ -1,9 +1,7 @@
 <?php
+
 return [
-
-    'option_repository' => env('WP2L_OPTION_REPO', 'db'),
-
     'options_table' => env('WP2L_OPTIONS_TABLE', 'wp_options'),
-
-    'option_cache_ttl' => env('WP2L_OPTION_CACHE_TTL', 0),
+    'option_cache_ttl' => env('WP2L_OPTION_CACHE_TTL', 0), // ttl cache base on sec (0 mean disable)
 ];
+

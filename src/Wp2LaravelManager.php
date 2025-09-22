e56b4543821b5b92187b557dbd266f0b45f368a4
reza_qsr <reza_qsr@yahoo.com>
2025-09-22 17:46:37 +0330

    Core Manager Modified For Options

--- src/Wp2LaravelManager.php
+++ src/Wp2LaravelManager.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace RezaQsr\Wp2Laravel;
+
+use RezaQsr\Wp2Laravel\Services\OptionService;
+
+class Wp2LaravelManager
+{
+    protected OptionService $options;
+
+    public function __construct(OptionService $options)
+    {
+        $this->options = $options;
+    }
+
+    public function getOption(string $key, $default = null)
+    {
+        return $this->options->getOption($key, $default);
+    }
+
+    public function updateOption(string $key, $value): bool
+    {
+        return $this->options->updateOption($key, $value);
+    }
+
+    public function deleteOption(string $key): bool
+    {
+        return $this->options->deleteOption($key);
+    }
+
+    public function addOption(string $key, $value, string $autoload = 'yes'): bool
+    {
+        return $this->options->addOption($key, $value, $autoload);
+    }
+}

9399c890bf22e699cd5ba44891b8e574390a6513
reza_qsr <reza_qsr@yahoo.com>
2025-09-26 19:30:27 +0330

    Option Model Added

---
+++ src/Models/Option.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RezaQsr\Wp2Laravel\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    protected $table;
+    protected $fillable = ['option_name', 'option_value', 'autoload'];
+
+    public $timestamps = false;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('wp2laravel.options_table', 'wp_options');
+    }
+}

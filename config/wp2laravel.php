c2f8b0246b931af13272dd8470698b8a2d90d2bc
reza_qsr <reza_qsr@yahoo.com>
2025-09-27 13:22:57 +0330

    config file modified

--- config/wp2laravel.php
+++ config/wp2laravel.php
@@ -2,7 +2,10 @@
 
 return [
     'options_table' => env('WP2L_OPTIONS_TABLE', 'wp_options'),
-    'post_table' => env('WP2L_POST_TABLE', 'wp_posts'),
+    'posts_table' => env('WP2L_POSTS_TABLE', 'wp_posts'),
     'postmeta_table' => env('WP2L_POSTMETA_TABLE', 'wp_postmeta'),
+    'terms_table' => env('WP2L_TERMS_TABLE', 'wp_terms'),
+    'term_relationships_table' => env('WP2L_TERM_RELATIONSHIPS_TABLE', 'wp_term_relationships'),
+    'term_taxonomy_table' => env('WP2L_TERM_TAXONOMY_TABLE', 'wp_term_taxonomy'),
 ];
 

5449310ee7316c979723781508db1f645804e721
reza_qsr <reza_qsr@yahoo.com>
2025-09-27 12:59:36 +0330

    config file modified

--- config/wp2laravel.php
+++ config/wp2laravel.php
@@ -2,5 +2,7 @@
 
 return [
     'options_table' => env('WP2L_OPTIONS_TABLE', 'wp_options'),
+    'post_table' => env('WP2L_POST_TABLE', 'wp_posts'),
+    'postmeta_table' => env('WP2L_POSTMETA_TABLE', 'wp_postmeta'),
 ];
 

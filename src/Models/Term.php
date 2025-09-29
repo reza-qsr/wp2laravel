7fdb1f55c75e83af73815784f4af6d6279a0f26c
reza_qsr <reza_qsr@yahoo.com>
2025-09-29 14:27:12 +0330

    fix Term Slug Duplicated Bug

--- src/Models/Term.php
+++ src/Models/Term.php
@@ -8,13 +8,28 @@ class Term extends Model
 {
     protected $primaryKey = 'term_id';
     protected $table;
-    protected $guarded = [];
+    protected $fillable = ['name', 'slug', 'term_group'];
     public $timestamps = false;
 
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->table = config('wp2laravel.terms_table', 'wp_terms');
+    }
+
+
+    protected static function booted()
+    {
+        static::creating(function (Term $term) {
+            $slug = $term->slug ?: Str::slug($term->name);
+
+            $baseSlug = $slug;
+            $i = 2;
+            while (Term::where('slug', $slug)->exists()) {
+                $slug = $baseSlug . '-' . $i++;
+            }
+            $term->slug = $slug;
+        });
     }
 
     public function taxonomies()

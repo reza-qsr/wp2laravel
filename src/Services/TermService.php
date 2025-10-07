0012dcf5ab5241663fc8467ef09c92f239fdc531
reza_qsr <reza_qsr@yahoo.com>
2025-10-07 22:16:59 +0330

    Term Service Updated

--- src/Services/TermService.php
+++ src/Services/TermService.php
@@ -17,14 +17,29 @@ class TermService
     {
         return $this->repo->getTerms($args);
     }
-
+    public function getTermBy(string $field, $value, string $taxonomy)
+    {
+        return $this->repo->getTermBy($field, $value, $taxonomy);
+    }
     public function insertTerm(string $term, string $taxonomy, array $args = [])
     {
         return $this->repo->insertTerm($term, $taxonomy, $args);
     }
-
+    public function updateTerm(int $termId, string $taxonomy, array $args = [])
+    {
+        return $this->repo->updateTerm($termId, $taxonomy, $args);
+    }
+    public function deleteTerm(int $termId, string $taxonomy)
+    {
+        return $this->repo->deleteTerm($termId, $taxonomy);
+    }
     public function setPostTerms(int $postId, array $terms, string $taxonomy, bool $append = false)
     {
         return $this->repo->setPostTerms($postId, $terms, $taxonomy, $append);
     }
+    public function getPostTerms(int $postId, string $taxonomy)
+    {
+        return $this->repo->getPostTerms($postId, $taxonomy);
+    }
+
 }

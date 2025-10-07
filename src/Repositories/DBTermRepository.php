3afa0e7129ccb10d8a238353da38f42431d0f26b
reza_qsr <reza_qsr@yahoo.com>
2025-10-07 22:05:34 +0330

    Term Functions Added

--- src/Repositories/DBTermRepository.php
+++ src/Repositories/DBTermRepository.php
@@ -29,6 +29,20 @@ class DBTermRepository implements TermRepositoryInterface
 
         return $query->get();
     }
+    public function getTermBy(string $field, $value, string $taxonomy)
+    {
+        $query = Term::query()->with('taxonomies');
+
+        if (!empty($taxonomy)) {
+            $query->whereHas('taxonomies', function ($q) use ($taxonomy) {
+                $q->where('taxonomy', $taxonomy);
+            });
+        }
+        if (!empty($field) && !empty($value)) {
+            $query->where($field, $value);
+        }
+        return $query->get();
+    }
     public function insertTerm(string $term, string $taxonomy, array $args = [])
     {
         $slug = $args['slug'] ?? Str::slug($term);
@@ -45,6 +59,24 @@ class DBTermRepository implements TermRepositoryInterface
         );
 
         return [$termModel, $taxonomyModel];
+    }
+
+    public function deleteTerm(int $termId, string $taxonomy): bool
+    {
+        $termTax = TermTaxonomy::where('term_id', $termId)
+            ->where('taxonomy', $taxonomy)
+            ->first();
+
+        if (!$termTax) return false;
+
+        TermRelationship::where('term_taxonomy_id', $termTax->term_taxonomy_id)->delete();
+        $termTax->delete();
+
+        if (!TermTaxonomy::where('term_id', $termId)->exists()) {
+            Term::find($termId)?->delete();
+        }
+
+        return true;
     }
     public function setPostTerms(int $postId, array $terms, string $taxonomy, bool $append = false)
     {

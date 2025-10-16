2d637e81228c742a5f2b685e3a03889a5f5e3917
reza_qsr <reza_qsr@yahoo.com>
2025-10-16 16:16:24 +0330

    User Models & Interface Updated

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace RezaQsr\Wp2Laravel\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table;
+    protected $primaryKey = 'ID';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_login', 'user_pass', 'user_nicename', 'user_email',
+        'user_url', 'user_registered', 'user_status', 'display_name'
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('wp2laravel.users_table', 'wp_users');
+    }
+    public function metas(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(UserMeta::class, 'user_id');
+    }
+}
--- src/Models/UserMeta.php
+++ src/Models/UserMeta.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace RezaQsr\Wp2Laravel\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserMeta extends Model
+{
+    protected $table;
+    protected $primaryKey = 'umeta_id';
+    public $timestamps = false;
+    protected $fillable = ['user_id', 'meta_key', 'meta_value'];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('wp2laravel.usermeta_table', 'wp_usermeta');
+    }
+}

b86eb8cef71f58dfadbece33fdb9d21013f6d46f
reza_qsr <reza_qsr@yahoo.com>
2025-10-16 16:12:48 +0330

    config Updated

--- config/wp2laravel.php
+++ config/wp2laravel.php
@@ -7,5 +7,7 @@ return [
     'terms_table' => env('WP2L_TERMS_TABLE', 'wp_terms'),
     'term_relationships_table' => env('WP2L_TERM_RELATIONSHIPS_TABLE', 'wp_term_relationships'),
     'term_taxonomy_table' => env('WP2L_TERM_TAXONOMY_TABLE', 'wp_term_taxonomy'),
+    'users_table' => env('WP2L_USER_TABLE', 'wp_users'),
+    'usermeta_table' => env('WP2L_USERMETA_TABLE', 'wp_usermeta'),
 ];
 

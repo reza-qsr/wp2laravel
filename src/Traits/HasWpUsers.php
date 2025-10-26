766cf619c2c677f2b001b45c2aa8e07dbc2ddf8a
reza_qsr <reza_qsr@yahoo.com>
2025-10-26 15:24:02 +0330

    Taxo Trait Added

--- src/Traits/HasWpUsers.php
+++ src/Traits/HasWpUsers.php
@@ -6,42 +6,42 @@ use RezaQsr\Wp2Laravel\Services\UserService;
 
 trait HasWpUsers
 {
-    protected function termsService(): UserService
+    protected function usersService(): UserService
     {
         return app(UserService::class);
     }
     public function insert_user(array $data)
     {
-        return $this->termsService()->insertUser($data);
+        return $this->usersService()->insertUser($data);
     }
     public function update_user(int $id, array $data): bool
     {
-        return $this->termsService()->updateUser($id, $data);
+        return $this->usersService()->updateUser($id, $data);
     }
     public function delete_user(int $id): bool
     {
-        return $this->termsService()->deleteUser($id);
+        return $this->usersService()->deleteUser($id);
     }
 
     public function get_user_meta(int $userId, string $key, $default = null)
     {
-        return $this->termsService()->getUserMeta($userId, $key, $default);
+        return $this->usersService()->getUserMeta($userId, $key, $default);
     }
     public function has_user_meta(int $userId, string $key)
     {
-        return $this->termsService()->hasUserMeta($userId, $key);
+        return $this->usersService()->hasUserMeta($userId, $key);
     }
     public function update_user_meta(int $userId, string $key, $value): bool
     {
-        return $this->termsService()->updateUserMeta($userId, $key, $value);
+        return $this->usersService()->updateUserMeta($userId, $key, $value);
     }
     public function delete_user_meta(int $userId, string $key): bool
     {
-        return $this->termsService()->deleteUserMeta($userId, $key);
+        return $this->usersService()->deleteUserMeta($userId, $key);
     }
 
     public function get_user_roles(int $userId): array
     {
-        return $this->termsService()->getUserRoles($userId);
+        return $this->usersService()->getUserRoles($userId);
     }
 }
